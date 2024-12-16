1e35da8f17ed360963db1340a5ee4a7bf2339203
zqz <961461851@qq.com>
2024-12-16 21:29:35 +0800

    [feat] Commit entry file.

--- Example1.cpp
+++ Example1.cpp
@@ -1,25 +1,16 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <mat4x4.hpp>
-#include <vec4.hpp>
+#include "Vulkan/app.h"
+#include <cstdlib>
 #include <iostream>
 
-int main() {
-  glfwInit();
-  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow *window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
-  uint32_t extensionCount = 0;
-  vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
-  std::cout << extensionCount << " extensions supported\n";
-  glm::mat4 matrix;
-  glm::vec4 vec;
-  auto test = matrix * vec;
-  while (!glfwWindowShouldClose(window)) {
-    glfwPollEvents();
-  }
-  glfwDestroyWindow(window);
-  glfwTerminate();
-  return 0;
+int main()
+{
+	vulkan_engine::App app;
+
+	try { app.Run(); }
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		return EXIT_FAILURE;
+	}
+	return EXIT_SUCCESS;
 }
